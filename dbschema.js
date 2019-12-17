c7116a461058b24829bde1742a2523a01f1ac3c2
Bui Kiem <buivantrung1105@gmail.com>
2019-12-17 17:47:20 +0700

    Create database schema

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+  screams: [
+    {
+      userHandle: "user",
+      body: "Scream body",
+      createdAt: "2019-12-17T10:40:55.024Z",
+      likeCount: 5,
+      commentCount: 3
+    }
+  ]
+}
