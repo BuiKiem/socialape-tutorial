751fbfdb71e963cc6029bc8183b3fadd96529a1b
Bui Kiem <buivantrung1105@gmail.com>
2019-12-17 18:08:03 +0700

    Implement signup api endpoint

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,23 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const express = require("express");
+const firebase = require("firebase");
 
-admin.initializeApp();
+
+const config = firebaseConfig = {
+  apiKey: "AIzaSyDYNEPe4jwYvT09TmMbWRW9kAI-nJRcOgs",
+  authDomain: "buikiem-socialape.firebaseapp.com",
+  databaseURL: "https://buikiem-socialape.firebaseio.com",
+  projectId: "buikiem-socialape",
+  storageBucket: "buikiem-socialape.appspot.com",
+  messagingSenderId: "275571777524",
+  appId: "1:275571777524:web:5f16d0b91eb9dfda5563a1",
+  measurementId: "G-3M1WR9XW3B"
+};
+
 const app = express();
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
+admin.initializeApp();
+firebase.initializeApp(config);
 
 app.get("/screams", (request, response) => {
   admin.firestore().collection("screams").orderBy("createdAt", "desc").get()
@@ -24,7 +34,6 @@ app.get("/screams", (request, response) => {
     .catch((error) => console.error(error));
 });
 
-
 app.post("/screams", (request, response) => {
   const newScream = {
     body: request.body.body,
@@ -39,6 +48,24 @@ app.post("/screams", (request, response) => {
     .catch(error => {
       console.log(error);
       return response.status(500).json({error: "Something went wrong"});
+    });
+});
+
+app.post("/signup", (request, response) => {
+  const newUser = {
+    email: request.body.email,
+    password: request.body.password,
+    confirmPassword: request.body.confirmPassword,
+    handle: request.body.handle,
+  }
+
+  //TODO: Validate data
+
+  firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password)
+    .then(data => response.status(201).json({message: `User ${data.user.uid} signup successfully!!!`}))
+    .catch(error => {
+      console.error(error);
+      return response.status(500).json({error: error.code});
     });
 });
 
